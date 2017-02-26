01867ce1349656190d9ad7ad03558800ba49e68f
Karen Bosch-Brakband <info@arvenzis.nl>
2017-02-26 14:24:10 +0100

    Initial commit

---
+++ src/Cutting.java
@@ -0,0 +1,20 @@
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+import org.powerbot.script.rt4.ClientContext;
+
+@Script.Manifest(
+        name = "Hello, RSBot!", properties = "author=Coma; topic=1296203; client=4;",
+        description = "A 'Hello, World' example for RSBot"
+)
+public class Cutting extends PollingScript<ClientContext> {
+
+    @Override
+    public void start() {
+        log.info("Hello, RSBot!");
+    }
+
+    @Override
+    public void poll() {
+
+    }
+}
