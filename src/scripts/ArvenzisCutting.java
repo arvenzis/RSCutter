d23f4629bf1a36d79207049f23c8c0ff8a1da784
Arvenzis <info@arvenzis.nl>
2017-03-12 13:14:31 +0100

    Bot can chop Falador trees now

---
+++ src/scripts/ArvenzisCutting.java
@@ -0,0 +1,28 @@
+package scripts;
+
+import org.powerbot.script.rt4.ClientContext;
+import org.powerbot.script.PollingScript;
+import org.powerbot.script.Script;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Script.Manifest( name = "ArvenzisCutting", description = "A simple cutting bot" )
+
+public class ArvenzisCutting extends PollingScript<ClientContext> {
+
+    private List<Task> taskList = new ArrayList<Task>();
+
+    public void start() {
+       taskList.addAll(Arrays.asList(new Chop(ctx)));
+    }
+
+    public void poll() {
+        for(Task task : taskList){
+            if(task.activate()){
+                task.execute();
+            }
+        }
+    }
+}
---
+++ src/scripts/Chop.java
@@ -0,0 +1,30 @@
+package scripts;
+
+import org.powerbot.script.rt4.ClientContext;
+import org.powerbot.script.rt4.GameObject;
+
+public class Chop extends Task<ClientContext>{
+    private int[] treeIds = {1276, 1278, 1271};
+
+    public Chop(ClientContext ctx){
+        super(ctx);
+    }
+
+    @Override
+    public boolean activate(){
+        return ctx.inventory.select().count() < 28
+                && !ctx.objects.select().id(treeIds).isEmpty()
+                && ctx.players.local().animation() == -1;
+    }
+
+    @Override
+    public void execute(){
+        GameObject tree = ctx.objects.nearest().poll();
+        if (tree.inViewport()){
+            tree.interact("Chop");
+        } else {
+            ctx.movement.step(tree);
+            ctx.camera.turnTo(tree);
+        }
+    }
+}
